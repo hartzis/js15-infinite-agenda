fb41d2d916253ccd9e85992a3c5b465481303862
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-17 17:03:04 -0600

    page layout and js comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+// declare variables
+
+
+// rendering functions
+
+var createDayTitle = function(dayDate) {
+
+
+}
+
+var createDayNote = function() {
+
+}
+
+var createDayContainer = function(dayDate) {
+
+}
+
+
 $(document).on('ready', function() {
-  
+
+    //create dom objects
+
+
+    //create functions
+
+
+
+
 });
